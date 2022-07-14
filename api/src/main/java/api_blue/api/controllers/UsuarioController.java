fb103ba86e6268e075f10de57ad8a327b5b003b9
miguel pc <miguel.reyesc@usantoto.edu.co>
2022-07-14 14:25:04 -0500

    api terminada

--- api/src/main/java/api_blue/api/controllers/UsuarioController.java
+++ api/src/main/java/api_blue/api/controllers/UsuarioController.java
@@ -1,12 +1,16 @@
 package api_blue.api.controllers;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import api_blue.api.models.UserModel;
@@ -26,5 +30,25 @@ public class UsuarioController {
     @PostMapping()
     public UserModel guardarUsuario(@RequestBody UserModel usuario) {
         return this.usuarioService.guardarUsuario(usuario);
+    }
+
+    @GetMapping(path = "/{id}")
+    public Optional<UserModel> obtenerUsuarioPorId(@PathVariable("id") Long id) {
+        return this.usuarioService.obtenerUsuariosId(id);
+    }
+
+    @GetMapping("/query")
+    public ArrayList<UserModel> obtenerUsuarioPorPrioridad(@RequestParam("prioridad") Integer prioridad) {
+        return this.usuarioService.obtenerUsuariosPorPrioridad(prioridad);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public String eliminarPorId(@PathVariable("id") Long id) {
+        boolean ok = this.usuarioService.eliminarUsuario(id);
+        if (ok) {
+            return "Usuario eliminado";
+        } else {
+            return "error no se puedo eliminar el usuario";
+        }
     }
 }
--- api/src/main/java/api_blue/api/services/UsuarioService.java
+++ api/src/main/java/api_blue/api/services/UsuarioService.java
@@ -1,6 +1,8 @@
 package api_blue.api.services;
 
+import java.io.Console;
 import java.util.ArrayList;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,6 +21,24 @@ public class UsuarioService {
 
     public UserModel guardarUsuario(UserModel usuario) {
         return usuarioRepository.save(usuario);
+    }
+
+    public Optional<UserModel> obtenerUsuariosId(Long id) {
+        return usuarioRepository.findById(id);
+    }
+
+    public ArrayList<UserModel> obtenerUsuariosPorPrioridad(Integer prioridad) {
+        return usuarioRepository.findByPrioridad(prioridad);
+    }
+
+    public boolean eliminarUsuario(Long id) {
+        try {
+            usuarioRepository.deleteById(id);
+            return true;
+        } catch (Exception e) {
+            // TODO: handle exception
+            return false;
+        }
     }
 
 }
